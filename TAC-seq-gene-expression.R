5f665c1f39f910c358878fe018a1f406cbe4ebbd
Hindrek Teder <hindrek.teder@gmail.com>
2020-07-31 14:34:44 +0300

    refactor code

--- TAC-seq-gene-expression.R
+++ TAC-seq-gene-expression.R
@@ -13,13 +13,11 @@ controls <- "TAC-seq-gene-expression/data/controls/READY65_control_set.tsv" %>%
   set_names(nm = basename(.)) %>%
   map_dfr(read_tsv, .id = "file")
 
-patients <- list.files("TAC-seq-gene-expression/data/counts/", pattern = "TAC-seq_counts2.tsv", full.names = TRUE) %>%
+patients <- list.files("TAC-seq-gene-expression/data/counts/", pattern = "TAC-seq_counts.tsv", full.names = TRUE) %>%
   set_names(nm = basename(.)) %>%
   map_dfr(read_tsv, .id = "file") %>%
-  filter(
-    !str_detect(sample, "Undetermined"),  # remove undetermined samples
-    locus != "unmatched"  # remove unmatched loci
-  ) %>%
+  filter(!str_detect(sample, "Undetermined"),  # remove undetermined samples
+         locus != "unmatched") %>%  # remove unmatched loci
   left_join(targets, by = c("locus" = "target"))
 
 
@@ -70,70 +68,18 @@ ggplotly()
 
 # normalize molecule counts -----------------------------------------------
 
-# lst <- df %>%
-#   select(sample, locus, molecule_count, type) %>%
-#   spread(sample, molecule_count) %>%
-#   split(.$type) %>%
-#   map(select, -type) %>%
-#   map(column_to_rownames, "locus") %>%
-#   map(as.matrix)
-#
-# bm_raw <- lst$biomarker
-# hk_raw <- lst$housekeeper
-#
-# norm_factor <- exp(apply(log(hk_raw), 2, mean))  # housekeepers geometric mean
-# bm <- sweep(bm_raw, 2, norm_factor, "/")
-
-patient_biomarkers <- patients %>%
+biomarkers <- patients %>%
   group_by(file, sample) %>%
   mutate(norm_factor = geo_mean(molecule_count[type == "housekeeper"])) %>%
   ungroup() %>%
   filter(type == "biomarker") %>%
-  mutate(norm_molecule_count = molecule_count / norm_factor)
-
-patient_biomarkers_clean <- patient_biomarkers %>%
-  filter(is.finite(norm_molecule_count))
-
-setdiff(patient_biomarkers$sample, patient_biomarkers_clean$sample)
-
-patient_biomarkers_wide <- patient_biomarkers_clean %>%
-  pivot_wider(id_cols = c(file, sample), names_from = locus, values_from = norm_molecule_count)
-
-biomarkers <- bind_rows(controls, patient_biomarkers_wide)
-
-
-# remove housekeeper outliers ---------------------------------------------
-
-# hk_zeros <- which(norm_factor == 0)
-# if (length(hk_zeros) > 0) {
-#   outliers <- names(hk_zeros)
-#   cat("removed samples:", outliers, sep = "\n")  # housekeepers geometric mean is 0
-#   bm <- bm[, -hk_zeros]
-# }
+  mutate(norm_molecule_count = molecule_count / norm_factor) %>%
+  filter(is.finite(norm_molecule_count)) %>%
+  pivot_wider(id_cols = c(file, sample), names_from = locus, values_from = norm_molecule_count) %>%
+  bind_rows(controls)
 
 
 # plot PCA ----------------------------------------------------------------
-
-# df <- bm %>%
-#   t() %>%
-#   as_tibble(rownames = "sample") %>%
-#   bind_rows(controls)
-#
-# mat <- df %>%
-#   select(-label) %>%
-#   column_to_rownames("sample") %>%
-#   as.matrix()
-#
-# pca <- mat %>%
-#   prcomp(scale. = T)
-#
-# pca$x %>%
-#   as_tibble(rownames = "sample") %>%
-#   left_join(df) %>%
-#   ggplot(aes(PC1, PC2, color = label, group = sample)) +
-#   geom_point() +
-#   labs(title = "Biomarkers", subtitle = "normalized molecule counts")
-# ggplotly()
 
 controls %>%
   recipe() %>%
@@ -165,8 +111,6 @@ ggplotly()
 
 # plot heatmap ------------------------------------------------------------
 
-# mat[mat == 0] <- NA  # replace 0 with NA
-# pheatmap(log(t(mat)), scale = "row", main = "Biomarkers", treeheight_row = 0)
 biomarkers %>%
   column_to_rownames("sample") %>%
   select(where(is.numeric)) %>%
